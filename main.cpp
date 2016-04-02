fb0f0414d0017bf33bbd2c7a851dfaa83b18df41
Rémi Martin <mrmi11@yahoo.fr>
2016-04-02 11:07:35 -0400

    Added main.cpp with compilation line in comments

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// g++ $(pkg-config --cflags --libs opencv) main.cpp -o Test 
+
+// OpenCV Diplay camera. http://talkera.org/opencv
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/tracking.hpp>
+ 
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+ 
+using namespace std;
+using namespace cv;
+ 
+int main( int argc, const char** argv )
+{
+    return 0;
+}
