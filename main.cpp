59a7b345847ca09cf9c2d4ada22c103d9a6dc3df
Rémi Martin <mrmi11@yahoo.fr>
2016-04-02 13:15:10 -0400

    Added fundamental matrix computation and epipolar line display

--- main.cpp
+++ main.cpp
@@ -18,6 +18,24 @@ using namespace cv;
 #define MIN_HESSIAN 800
 /* ----- DEFINES ----- END */
  
+void drawEpilines(Mat & img, vector< Vec3f> lines, vector< Point2f > points)
+{
+    for (std::vector<Vec3f>::const_iterator it = lines.begin(); it!=lines.end(); ++it)
+    {
+        cv::line(
+            img,
+            Point(0,-(*it)[2]/(*it)[1]),
+            Point(img.cols,-((*it)[2]+(*it)[0]*img.cols)/(*it)[1]),
+            Scalar(255,255,255)
+        );
+    }
+
+    for (std::vector<Point2f>::const_iterator pt = points.begin(); pt!=points.end(); ++pt)
+    {
+        circle(img, *pt, 5, Scalar(255,0,0));
+    }
+}
+
 int main( int argc, const char** argv )
 {
     /* ----- AUTO CALIBRATION ----- BEGIN */
@@ -40,6 +58,45 @@ int main( int argc, const char** argv )
     FlannBasedMatcher matcher;
     std::vector< DMatch > matches;
     matcher.match( desc_left, desc_right, matches );
+
+    // Select good matches
+    vector< DMatch > good_matches;
+    vector< Point2f > left;
+    vector< Point2f > right;
+    double max_dist = 0; double min_dist = 100;
+    DMatch current_match;
+
+    for( int i = 0; i < desc_left.rows; i++ ) // Quick calculation of max and min distances between keypoints
+    { 
+        double dist = matches[i].distance;
+        if( dist < min_dist ) min_dist = dist;
+        if( dist > max_dist ) max_dist = dist;
+    }
+
+    for( int i = 0; i < desc_left.rows; i++ )
+    { 
+        if( matches[i].distance < 3*min_dist )
+        {
+            current_match = matches[i];
+            good_matches.push_back( current_match );
+            left.push_back( kp_left[ current_match.queryIdx ].pt );
+            right.push_back( kp_right[ current_match.trainIdx ].pt );
+        }
+    }
+
+    // Calculate fundamental matrix
+    Mat F = findFundamentalMat(Mat(left),Mat(right),CV_FM_LMEDS);
+
+    // Find epilines 
+    vector< Vec3f > lines_left, lines_right;
+    computeCorrespondEpilines(right, 2, F, lines_left);
+    drawEpilines(leftImage, lines_left, left);
+    computeCorrespondEpilines(left, 1, F, lines_right);
+    drawEpilines(rightImage, lines_right, right);
+    
+    imshow("left", leftImage);
+    imshow("right", rightImage);
+    waitKey(0);
 
     return 0;
 }
