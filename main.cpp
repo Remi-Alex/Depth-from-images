de454238246de9a9effbb19b02d54825c7e43791
Rémi Martin <mrmi11@yahoo.fr>
2016-04-02 11:34:34 -0400

    Added keypoints calcultation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 // g++ $(pkg-config --cflags --libs opencv) main.cpp -o Test 
 
-// OpenCV Diplay camera. http://talkera.org/opencv
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/video/tracking.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
  
 #include <iostream>
 #include <stdio.h>
@@ -12,8 +13,33 @@
  
 using namespace std;
 using namespace cv;
+
+/* ----- DEFINES ----- BEGIN */
+#define MIN_HESSIAN 800
+/* ----- DEFINES ----- END */
  
 int main( int argc, const char** argv )
 {
+    /* ----- AUTO CALIBRATION ----- BEGIN */
+    Mat leftImage = imread("images/left.jpg"); // query
+    Mat rightImage = imread("images/right.jpg"); // train
+
+    // Keypoints
+    SiftFeatureDetector detector(MIN_HESSIAN);
+    std::vector< KeyPoint > kp_left, kp_right;
+    detector.detect(leftImage, kp_left);
+    detector.detect(rightImage, kp_right);
+    
+    // Descriptors
+    SiftDescriptorExtractor extractor;
+    Mat desc_left, desc_right;
+    extractor.compute( leftImage, kp_left, desc_left );
+    extractor.compute( rightImage, kp_right, desc_right );
+
+    // FLANN matches
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
+    matcher.match( desc_left, desc_right, matches );
+
     return 0;
 }
